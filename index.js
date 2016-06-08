73dfae9f31a52028fc2a7a9f61ce45709f9abf86
justin <justinpeck7@gmail.com>
2016-06-08 02:41:30 -0500

    update regex to match whitespace padded zipcodes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const secrets = require('./secrets'),
     botkit = require('botkit'),
     request = require('request'),
     controller = botkit.slackbot(),
-    ziptest = /\d{5}/g,
+    ziptest = /\s(\d{5})(?=\s)/g, //why doesn't javascript regex support lookbehind?
     weatherman = controller.spawn({
         token: token
     }).startRTM();
@@ -16,7 +16,8 @@ controller.on('direct_mention,direct_message', (bot, msg) => {
     const matches = msg.text.match(ziptest);
 
     if (!!matches) {
-        for (const zip of matches) {
+        for (let zip of matches) {
+            zip = zip.replace(/\s/, '');
             request(`http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${api}&units=imperial`, function(err, response, body) {
                 let info = JSON.parse(body);
                 bot.reply(msg, `Weather in ${info.name}: ${info.main.temp} degrees, ${info.weather[0].description}, wind speed is ${info.wind.speed}mph`);

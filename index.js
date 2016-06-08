6b1787687910bba84d2929c3a7bc5f727a374023
justin <justinpeck7@gmail.com>
2016-06-08 03:12:21 -0500

    small syntax changes, update package.json

--- index.js
+++ index.js
@@ -12,17 +12,16 @@ const secrets = require('./secrets'),
     }).startRTM();
 
 controller.on('direct_mention,direct_message', (bot, msg) => {
-
+    
     const matches = msg.text.match(ziptest);
 
     if (!!matches) {
-        for (let zip of matches) {
-            request(`http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${api}&units=imperial`, function(err, response, body) {
-                let info = JSON.parse(body);
+        for (const zip of matches) {
+            request(`http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${api}&units=imperial`, (err, response, body) => {
+                const info = JSON.parse(body);
                 bot.reply(msg, `Weather in ${info.name}: ${info.main.temp} degrees, ${info.weather[0].description}, wind speed is ${info.wind.speed}mph`);
             });
         }
-
     } else {
         bot.reply(msg, 'Give me a zipcode ya idiot');
     }

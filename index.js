501038639ea7f42a17ef725e5842b365181385a2
justin <justinpeck7@gmail.com>
2016-06-08 01:15:32 -0500

    actually support multiple zips

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const secrets = require('./secrets'),
     botkit = require('botkit'),
     request = require('request'),
     controller = botkit.slackbot(),
-    ziptest = /\d{5}/,
+    ziptest = /\d{5}/g,
     weatherman = controller.spawn({
         token: token
     }).startRTM();
@@ -16,7 +16,7 @@ controller.on('direct_mention,direct_message', (bot, msg) => {
     const matches = msg.text.match(ziptest);
 
     if (!!matches) {
-        for (zip of matches) {
+        for (const zip of matches) {
             request(`http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${api}&units=imperial`, function(err, response, body) {
                 let info = JSON.parse(body);
                 bot.reply(msg, `Weather in ${info.name}: ${info.main.temp} degrees, ${info.weather[0].description}, wind speed is ${info.wind.speed}mph`);

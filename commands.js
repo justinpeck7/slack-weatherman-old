82edf08653a837b89fd666b110c1c258ed473103
justin <justinpeck7@gmail.com>
2016-06-10 17:19:04 -0500

    only respond when text starts with command

--- commands.js
+++ commands.js
@@ -20,7 +20,7 @@ request(`https://slack.com/api/channels.list?token=${token}`, (cErr, cResponse, 
     request(`https://slack.com/api/groups.list?token=${token}`, (gErr, gResponse, gBody) => {
         channels = JSON.parse(cBody).channels;
         let groups = JSON.parse(gBody).groups;
-        for(let group of groups) {
+        for (let group of groups) {
             channels.push(group);
         }
         console.log(`${channels.length} Channels Found`);
@@ -28,89 +28,97 @@ request(`https://slack.com/api/channels.list?token=${token}`, (cErr, cResponse, 
 });
 
 let weather = (bot, message) => {
-    const matches = message.text.match(ziptest);
+    if (message.text.split(' ')[0] === '!weather') {
+        const matches = message.text.match(ziptest);
 
-    if (!!matches) {
-        for (const zip of matches) {
-            request(`http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${api}&units=imperial`, (err, response, body) => {
-                const data = JSON.parse(body);
-                bot.reply(message, `Weather in ${data.name}: ${data.main.temp} degrees, ${data.weather[0].description}, wind speed is ${data.wind.speed}mph`);
-            });
+        if (!!matches) {
+            for (const zip of matches) {
+                request(`http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${api}&units=imperial`, (err, response, body) => {
+                    const data = JSON.parse(body);
+                    bot.reply(message, `Weather in ${data.name}: ${data.main.temp} degrees, ${data.weather[0].description}, wind speed is ${data.wind.speed}mph`);
+                });
+            }
         }
     }
 };
 
 let forecast = (bot, message) => {
-    let day = message.text.split(' ')[1].toUpperCase() || '',
-        zip = message.text.split(' ')[2] || '',
-        forecastData;
+    if (message.text.split(' ')[0] === '!forecast') {
+        let day = message.text.split(' ')[1].toUpperCase() || '',
+            zip = message.text.split(' ')[2] || '',
+            forecastData;
 
-    if (days[day] === undefined || !zip.match(ziptest)) {
-        bot.reply(message, 'Usage:');
-        bot.reply(message, '!forecast {day - within 4 of current day} {zipcode}');
-    }
-    else {
-        request(`http://api.openweathermap.org/data/2.5/forecast?zip=${zip},us&appid=${api}&units=imperial`, (err, response, body) => {
-            const data = JSON.parse(body),
-                city = data.city.name;
+        if (days[day] === undefined || !zip.match(ziptest)) {
+            bot.reply(message, 'Usage:');
+            bot.reply(message, '!forecast {day - within 4 of current day} {zipcode}');
+        }
+        else {
+            request(`http://api.openweathermap.org/data/2.5/forecast?zip=${zip},us&appid=${api}&units=imperial`, (err, response, body) => {
+                const data = JSON.parse(body),
+                    city = data.city.name;
 
-            for (const hourly of data.list) {
-                const d = new Date(hourly.dt_txt);
+                for (const hourly of data.list) {
+                    const d = new Date(hourly.dt_txt);
 
-                if (d.getDay() === days[day] && d.getHours() === 21) {
-                    forecastData = hourly;
-                    break;
+                    if (d.getDay() === days[day] && d.getHours() === 21) {
+                        forecastData = hourly;
+                        break;
+                    }
                 }
-            }
 
-            if (!!forecastData) {
-                bot.reply(message, `It will be ${forecastData.main.temp_max} degrees in ${city} on ${day.charAt(0) + day.slice(1).toLowerCase()}`);
-            }
-            else {
-                bot.reply(message, 'No data');
-            }
-        });
+                if (!!forecastData) {
+                    bot.reply(message, `It will be ${forecastData.main.temp_max} degrees in ${city} on ${day.charAt(0) + day.slice(1).toLowerCase()}`);
+                }
+                else {
+                    bot.reply(message, 'No data');
+                }
+            });
+        }
     }
 };
 
 let evaluate = (bot, message) => {
-    try {
-        const strToEval = message.text
-            .replace(/(!eval\b)/g, '')
-            .replace(/&lt;/g, '<')
-            .replace(/&gt;/g, '>')
-            .replace(/&amp;/g, '&')
-            .replace(/‘/g, '\'')
-            .replace(/“/g, '"'),
-            response = eval(strToEval);
+    if (message.text.split(' ')[0] === '!eval') {
+        try {
+            const strToEval = message.text
+                .replace(/(!eval\b)/g, '')
+                .replace(/&lt;/g, '<')
+                .replace(/&gt;/g, '>')
+                .replace(/&amp;/g, '&')
+                .replace(/‘/g, '\'')
+                .replace(/“/g, '"'),
+                response = eval(strToEval);
 
-        bot.reply(message, `${response}`);
-    } catch (error) {
-        bot.reply(message, `${error}`);
+            bot.reply(message, `${response}`);
+        } catch (error) {
+            bot.reply(message, `${error}`);
+        }
     }
 };
 
 let say = (bot, message) => {
-    let [inputChannel, ...rest] = message.text.split(' ').splice(1),
-        text = rest.join(' '),
-        sayChannel;
+    if (message.text.split(' ')[0] === '!say') {
+        let [inputChannel, ...rest] = message.text.split(' ').splice(1),
+            text = rest.join(' '),
+            sayChannel;
 
-    for (const channel of channels) {
-        if (channel.name.toUpperCase() === inputChannel.toUpperCase()) {
-            sayChannel = channel;
-            break;
+        for (const channel of channels) {
+            if (channel.name.toUpperCase() === inputChannel.toUpperCase()) {
+                sayChannel = channel;
+                break;
+            }
         }
-    }
 
-    if (!!sayChannel) {
-        bot.say({
-            text: text,
-            channel: sayChannel.id
-        });
-        bot.reply(message, 'Sent');
-    }
-    else {
-        bot.reply(message, 'Channel not found');
+        if (!!sayChannel) {
+            bot.say({
+                text: text,
+                channel: sayChannel.id
+            });
+            bot.reply(message, 'Sent');
+        }
+        else {
+            bot.reply(message, 'Channel not found');
+        }
     }
 };
 

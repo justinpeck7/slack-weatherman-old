3a0558d6cd0d137e31dbf00e528fcaf1a36efe39
Justin Peck <justinpeck7@gmail.com>
2018-05-15 20:35:38 -0500

    use winston for logging

--- src/botstart.js
+++ src/botstart.js
@@ -1,40 +1,54 @@
-'use strict';
+"use strict";
 
-const secrets = require('./config/secrets'),
-    commands = require('./commands/commands'),
-    token = secrets.slack_token,
-    botkit = require('botkit'),
-    dns = require('dns'),
-    logStream = require('fs').createWriteStream(require('path').resolve(__dirname, 'logging/log.txt')),
-    controller = botkit.slackbot();
+const secrets = require("./config/secrets");
+const commands = require("./commands/commands");
+const token = secrets.slack_token;
+const botkit = require("botkit");
+const dns = require("dns");
+const controller = botkit.slackbot();
+const { createLogger, format, transports } = require("winston");
+const { combine, timestamp, printf } = format;
 
 let weatherman;
 
+const loggerFormat = printf(info => {
+  return `${info.timestamp}: ${info.message}`;
+});
+
+const logger = createLogger({
+  format: combine(timestamp(), loggerFormat),
+  transports: [
+    new transports.File({ filename: "error.log", options: { flags: "w" } })
+  ]
+});
+
 const startBot = () => {
-    logStream.write('Starting\n');
-    weatherman = controller.spawn({
-        token: token
-    }).startRTM();
+  logger.info("Starting...");
+  weatherman = controller
+    .spawn({
+      token: token
+    })
+    .startRTM();
 };
 
 startBot();
 
-controller.on('rtm_close', () => {
-    logStream.write(`rtm_close on ${new Date()}\n`)
-    const intervalID = setInterval(() => {
-        logStream.write('Retrying connection...\n')
-        dns.lookupService('8.8.8.8', 80, (error, hostname) => {
-            if (!!hostname) {
-                logStream.write('Connection found, restarting bot...\n');
-                startBot();
-                clearInterval(intervalID);
-            }
-        });
-    }, 30000);
+controller.on("rtm_close", () => {
+  logger.info("RTM close");
+  const intervalID = setInterval(() => {
+    logger.info("Retrying connection");
+    dns.lookupService("8.8.8.8", 80, (error, hostname) => {
+      if (!!hostname) {
+        logger.info("Connection found, restarting");
+        startBot();
+        clearInterval(intervalID);
+      }
+    });
+  }, 60000);
 });
 
-controller.hears('!commands', 'ambient,direct_message', (bot, message) => {
-    const cmds = `How to order me around:
+controller.hears("!commands", "ambient,direct_message", (bot, message) => {
+  const cmds = `How to order me around:
     !weather [zipcodes]
     !forecast [day] [zipcode]
     !define [word]
@@ -47,18 +61,22 @@ controller.hears('!commands', 'ambient,direct_message', (bot, message) => {
     !question -- show current question
     !answer -- show answer to current question`;
 
-    bot.reply(message, cmds);
+  bot.reply(message, cmds);
 });
 
-controller.on('direct_mention', commands.sendMessage);
-controller.hears('!weather', 'ambient,direct_message', commands.weather);
-controller.hears('!forecast', 'ambient,direct_message', commands.forecast);
-controller.hears('!eval', 'ambient,direct_message', commands.evaluate);
-controller.hears('!define', 'ambient,direct_message', commands.define);
-controller.hears('!say', 'direct_message', commands.say);
-controller.hears('!leaderboard', 'ambient,direct_message', commands.showLeaderboard);
-controller.hears('\\+\\+', 'ambient,direct_message', commands.addRep);
-controller.hears('--', 'ambient,direct_message', commands.subtractRep);
-controller.hears('!trebek', 'ambient,direct_message', commands.getNewQuestion);
-controller.hears('!question', 'ambient,direct_message', commands.getQuestion);
-controller.hears('!answer', 'ambient,direct_message', commands.getAnswer);
+controller.on("direct_mention", commands.sendMessage);
+controller.hears("!weather", "ambient,direct_message", commands.weather);
+controller.hears("!forecast", "ambient,direct_message", commands.forecast);
+controller.hears("!eval", "ambient,direct_message", commands.evaluate);
+controller.hears("!define", "ambient,direct_message", commands.define);
+controller.hears("!say", "direct_message", commands.say);
+controller.hears(
+  "!leaderboard",
+  "ambient,direct_message",
+  commands.showLeaderboard
+);
+controller.hears("\\+\\+", "ambient,direct_message", commands.addRep);
+controller.hears("--", "ambient,direct_message", commands.subtractRep);
+controller.hears("!trebek", "ambient,direct_message", commands.getNewQuestion);
+controller.hears("!question", "ambient,direct_message", commands.getQuestion);
+controller.hears("!answer", "ambient,direct_message", commands.getAnswer);
--- src/commands/commands.js
+++ src/commands/commands.js
@@ -1,248 +1,287 @@
-'use strict';
+"use strict";
 
-const request = require('request'),
-    secrets = require('../config/secrets'),
-    noresults = require('../config/noresults'),
-    services = require('../services/services'),
-    safeEval = require('safe-eval'),
-    appId = secrets.weather_appid,
-    token = secrets.slack_token,
-    ziptest = /(\b\d{5}\b)/g,
-    parser = require('../plusplus/utils/parser'),
-    plusplus = require('../plusplus/plusplus'),
-    botlibre = require('../botlibre/botlibre'),
-    jeopardy = require('../trivia/jeopardy'),
-    days = {
-        SUNDAY: 0,
-        MONDAY: 1,
-        TUESDAY: 2,
-        WEDNESDAY: 3,
-        THURSDAY: 4,
-        FRIDAY: 5,
-        SATURDAY: 6
-    };
+const request = require("request");
+const secrets = require("../config/secrets");
+const noresults = require("../config/noresults");
+const services = require("../services/services");
+const safeEval = require("safe-eval");
+const appId = secrets.weather_appid;
+const token = secrets.slack_token;
+const ziptest = /(\b\d{5}\b)/g;
+const parser = require("../plusplus/utils/parser");
+const plusplus = require("../plusplus/plusplus");
+const botlibre = require("../botlibre/botlibre");
+const jeopardy = require("../trivia/jeopardy");
+const days = {
+  SUNDAY: 0,
+  MONDAY: 1,
+  TUESDAY: 2,
+  WEDNESDAY: 3,
+  THURSDAY: 4,
+  FRIDAY: 5,
+  SATURDAY: 6
+};
 
-let channels, users = {};
+let channels,
+  users = {};
 
 request(services.slackChannelApi(token), (cErr, cResponse, cBody) => {
-    request(services.slackGroupApi(token), (gErr, gResponse, gBody) => {
-        channels = JSON.parse(cBody).channels;
-        let groups = JSON.parse(gBody).groups;
-        for (let group of groups) {
-            channels.push(group);
-        }
-        request(services.slackUsers(token), (uErr, uResponse, uBody) => {
-            const members = JSON.parse(uBody).members;
-            for (const member of members) {
-                users[member.id] = member.name;
-            }
-            console.log(`${Object.keys(users).length} Users Found across ${channels.length} channels`);
-        });
+  request(services.slackGroupApi(token), (gErr, gResponse, gBody) => {
+    channels = JSON.parse(cBody).channels;
+    let groups = JSON.parse(gBody).groups;
+    for (let group of groups) {
+      channels.push(group);
+    }
+    request(services.slackUsers(token), (uErr, uResponse, uBody) => {
+      const members = JSON.parse(uBody).members;
+      for (const member of members) {
+        users[member.id] = member.name;
+      }
+      console.log(
+        `${Object.keys(users).length} Users Found across ${
+          channels.length
+        } channels`
+      );
     });
+  });
 });
 
 let weather = (bot, message) => {
-    if (message.text.split(' ')[0].toLowerCase() === '!weather') {
-        const matches = message.text.match(ziptest);
+  if (message.text.split(" ")[0].toLowerCase() === "!weather") {
+    const matches = message.text.match(ziptest);
 
-        if (!!matches) {
-            for (const zip of matches) {
-                request(services.weatherDataApi(zip, appId), (err, response, body) => {
-                    try {
-                        const data = JSON.parse(body);
-                        bot.reply(message, `Weather in ${data.name}: ${data.main.temp} degrees, ${data.weather[0].description}, wind speed is ${data.wind.speed}mph`);
-                    } catch (error) {
-                        bot.reply(message, 'Server error, not my fault. Try again later I guess.');
-                    }
-                });
-            }
-        }
+    if (!!matches) {
+      for (const zip of matches) {
+        request(services.weatherDataApi(zip, appId), (err, response, body) => {
+          try {
+            const data = JSON.parse(body);
+            bot.reply(
+              message,
+              `Weather in ${data.name}: ${data.main.temp} degrees, ${
+                data.weather[0].description
+              }, wind speed is ${data.wind.speed}mph`
+            );
+          } catch (error) {
+            bot.reply(
+              message,
+              "Server error, not my fault. Try again later I guess."
+            );
+          }
+        });
+      }
     }
+  }
 };
 
 let forecast = (bot, message) => {
-    if (message.text.split(' ')[0].toLowerCase() === '!forecast') {
-        let day = message.text.split(' ')[1].toUpperCase() || '',
-            zip = message.text.split(' ')[2] || '',
-            forecastData;
+  if (message.text.split(" ")[0].toLowerCase() === "!forecast") {
+    let day = message.text.split(" ")[1].toUpperCase() || "",
+      zip = message.text.split(" ")[2] || "",
+      forecastData;
 
-        if (days[day] === undefined || !zip.match(ziptest)) {
-            bot.reply(message, 'Usage:');
-            bot.reply(message, '!forecast {day - within 4 of current day} {zipcode}');
+    if (days[day] === undefined || !zip.match(ziptest)) {
+      bot.reply(message, "Usage:");
+      bot.reply(message, "!forecast {day - within 4 of current day} {zipcode}");
+    } else {
+      request(services.forecastDataApi(zip, appId), (err, response, body) => {
+        try {
+          const data = JSON.parse(body),
+            city = data.city.name;
+
+          for (const hourly of data.list) {
+            const d = new Date(hourly.dt_txt);
+
+            if (d.getDay() === days[day] && d.getHours() === 21) {
+              forecastData = hourly;
+              break;
+            }
+          }
+          if (!!forecastData) {
+            bot.reply(
+              message,
+              `It will be ${
+                forecastData.main.temp_max
+              } degrees in ${city} on ${day.charAt(0) +
+                day.slice(1).toLowerCase()}`
+            );
+          } else {
+            bot.reply(message, "No data");
+          }
+        } catch (error) {
+          bot.reply(
+            message,
+            "Server error, not my fault. Try again later I guess."
+          );
         }
-        else {
-            request(services.forecastDataApi(zip, appId), (err, response, body) => {
-                try {
-                    const data = JSON.parse(body),
-                        city = data.city.name;
-
-                    for (const hourly of data.list) {
-                        const d = new Date(hourly.dt_txt);
-
-                        if (d.getDay() === days[day] && d.getHours() === 21) {
-                            forecastData = hourly;
-                            break;
-                        }
-                    }
-                    if (!!forecastData) {
-                        bot.reply(message, `It will be ${forecastData.main.temp_max} degrees in ${city} on ${day.charAt(0) + day.slice(1).toLowerCase()}`);
-                    }
-                    else {
-                        bot.reply(message, 'No data');
-                    }
-                } catch (error) {
-                    bot.reply(message, 'Server error, not my fault. Try again later I guess.');
-                }
-            });
-        }
+      });
     }
+  }
 };
 
 let evaluate = (bot, message) => {
-    if (message.text.split(' ')[0].toLowerCase() === '!eval') {
-        try {
-            const strToEval = message.text
-                .replace(/(!eval\b)/g, '')
-                .replace(/&lt;/g, '<')
-                .replace(/&gt;/g, '>')
-                .replace(/&amp;/g, '&')
-                .replace(/‘/g, '\'')
-                .replace(/“/g, '"'),
-                response = safeEval(strToEval);
+  if (message.text.split(" ")[0].toLowerCase() === "!eval") {
+    try {
+      const strToEval = message.text
+          .replace(/(!eval\b)/g, "")
+          .replace(/&lt;/g, "<")
+          .replace(/&gt;/g, ">")
+          .replace(/&amp;/g, "&")
+          .replace(/‘/g, "'")
+          .replace(/“/g, '"'),
+        response = safeEval(strToEval);
 
-            bot.reply(message, `${response}`);
-        } catch (error) {
-            bot.reply(message, `${error}`);
-        }
+      bot.reply(message, `${response}`);
+    } catch (error) {
+      bot.reply(message, `${error}`);
     }
+  }
 };
 
 let define = (bot, message) => {
-    if (message.text.split(' ')[0].toLowerCase() === '!define') {
-        const words = message.text.split(' ').splice(1).join(' ');
+  if (message.text.split(" ")[0].toLowerCase() === "!define") {
+    const words = message.text
+      .split(" ")
+      .splice(1)
+      .join(" ");
 
-        request(services.urbanDictApi(words), (err, response, body) => {
-            const data = JSON.parse(body);
+    request(services.urbanDictApi(words), (err, response, body) => {
+      const data = JSON.parse(body);
 
-            if (data.result_type === 'exact') {
-                bot.reply(message, `"${data.list[0].definition}"`);
-                if (data.tags.length > 0) {
-                    bot.reply(message, `Related: ${data.tags.join(', ')}`);
-                }
-            }
-            else {
-                let response = noresults.responses[Math.random() * noresults.responses.length >> 0];
-                bot.reply(message, response);
-            }
-        });
-    }
+      if (data.result_type === "exact") {
+        bot.reply(message, `"${data.list[0].definition}"`);
+        if (data.tags.length > 0) {
+          bot.reply(message, `Related: ${data.tags.join(", ")}`);
+        }
+      } else {
+        let response =
+          noresults.responses[
+            (Math.random() * noresults.responses.length) >> 0
+          ];
+        bot.reply(message, response);
+      }
+    });
+  }
 };
 
 let say = (bot, message) => {
-    if (message.text.split(' ')[0].toLowerCase() === '!say') {
-        let [inputChannel, ...rest] = message.text.split(' ').splice(1),
-            text = rest.join(' '),
-            sayChannel;
+  if (message.text.split(" ")[0].toLowerCase() === "!say") {
+    let [inputChannel, ...rest] = message.text.split(" ").splice(1),
+      text = rest.join(" "),
+      sayChannel;
 
-        for (const channel of channels) {
-            if (channel.name.toUpperCase() === inputChannel.toUpperCase()) {
-                sayChannel = channel;
-                break;
-            }
-        }
-
-        if (!!sayChannel) {
-            bot.say({
-                text: text,
-                channel: sayChannel.id
-            });
-            bot.reply(message, 'Sent');
-        }
-        else {
-            bot.reply(message, 'Channel not found');
-        }
+    for (const channel of channels) {
+      if (channel.name.toUpperCase() === inputChannel.toUpperCase()) {
+        sayChannel = channel;
+        break;
+      }
     }
+
+    if (!!sayChannel) {
+      bot.say({
+        text: text,
+        channel: sayChannel.id
+      });
+      bot.reply(message, "Sent");
+    } else {
+      bot.reply(message, "Channel not found");
+    }
+  }
 };
 
 const addRep = (bot, message) => {
-    if (parser.isValidInput(message.text.split(' '))) {
-        plusplus.updateRep(message, users, true).then(updated => {
-            if (updated.sameUser) {
-                bot.reply(message, `${updated.user}'s rep decreased to ${updated.rank}`);
-            }
-            else {
-                bot.reply(message, `${updated.user}'s rep increased to ${updated.rank}`);
-            }
-        });
-    }
+  if (parser.isValidInput(message.text.split(" "))) {
+    plusplus.updateRep(message, users, true).then(updated => {
+      if (updated.sameUser) {
+        bot.reply(
+          message,
+          `${updated.user}'s rep decreased to ${updated.rank}`
+        );
+      } else {
+        bot.reply(
+          message,
+          `${updated.user}'s rep increased to ${updated.rank}`
+        );
+      }
+    });
+  }
 };
 
 const subtractRep = (bot, message) => {
-    if (parser.isValidInput(message.text.split(' '))) {
-        plusplus.updateRep(message, users, false).then(updated => {
-            bot.reply(message, `${updated.user}'s rep decreased to ${updated.rank}`);
-        });
-    }
+  if (parser.isValidInput(message.text.split(" "))) {
+    plusplus.updateRep(message, users, false).then(updated => {
+      bot.reply(message, `${updated.user}'s rep decreased to ${updated.rank}`);
+    });
+  }
 };
 
 const showLeaderboard = (bot, message) => {
-    const sortable = [],
-        space = ' ',
-        divider = '|';
+  const sortable = [],
+    space = " ",
+    divider = "|";
 
-    plusplus.getRanks().then(stored => {
-        for (const key in stored) {
-            sortable.push([key, stored[key]]);
-        }
-        sortable.sort((a, b) => b[1] - a[1]);
-        bot.reply(message, 'Rankings:');
-        bot.reply(message, '-'.repeat(20));
-        for (const entry of sortable) {
-            bot.reply(message, `${entry[1]}${space.repeat(Math.abs(4 - entry[1].toString().length))}${divider} ${entry[0]}`);
-        }
-    });
+  plusplus.getRanks().then(stored => {
+    for (const key in stored) {
+      sortable.push([key, stored[key]]);
+    }
+    sortable.sort((a, b) => b[1] - a[1]);
+    bot.reply(message, "Rankings:");
+    bot.reply(message, "-".repeat(20));
+    for (const entry of sortable) {
+      bot.reply(
+        message,
+        `${entry[1]}${space.repeat(
+          Math.abs(4 - entry[1].toString().length)
+        )}${divider} ${entry[0]}`
+      );
+    }
+  });
 };
 
 const sendMessage = (bot, message) => {
-    botlibre.sendMessage(message.text).then(response => {
-        bot.reply(message, response);
-    })
+  botlibre.sendMessage(message.text).then(response => {
+    bot.reply(message, response);
+  });
 };
 
 const getNewQuestion = (bot, message) => {
-    jeopardy.getNewQuestion().then(response => {
-        bot.reply(message, response);
-    }).catch((err) => {
-        console.log(err);
+  jeopardy
+    .getNewQuestion()
+    .then(response => {
+      bot.reply(message, response);
+    })
+    .catch(err => {
+      console.log(err);
     });
 };
 
 const getQuestion = (bot, message) => {
-    const currentQ = jeopardy.getQuestion(),
-        response = `"${currentQ.question}"\n\nOriginally aired ${currentQ.airdate}, was worth ${currentQ.points} points`;
-    bot.reply(message, response);
+  const currentQ = jeopardy.getQuestion(),
+    response = `"${currentQ.question}"\n\nOriginally aired ${
+      currentQ.airdate
+    }, was worth ${currentQ.points} points`;
+  bot.reply(message, response);
 };
 
 const getAnswer = (bot, message) => {
-    bot.reply(message, jeopardy.getAnswer());
+  bot.reply(message, jeopardy.getAnswer());
 };
 
 const image = (bot, message) => {
-    //AIzaSyCDQH0vOy2RLKp4tl8P_VOED540NQFi81M
+  //AIzaSyCDQH0vOy2RLKp4tl8P_VOED540NQFi81M
 };
 
 module.exports = {
-    weather,
-    forecast,
-    evaluate,
-    define,
-    say,
-    addRep,
-    subtractRep,
-    showLeaderboard,
-    sendMessage,
-    getNewQuestion,
-    getQuestion,
-    getAnswer,
-    image
+  weather,
+  forecast,
+  evaluate,
+  define,
+  say,
+  addRep,
+  subtractRep,
+  showLeaderboard,
+  sendMessage,
+  getNewQuestion,
+  getQuestion,
+  getAnswer,
+  image
 };
